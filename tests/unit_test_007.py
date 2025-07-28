de94a61d2aefc8712845055f265de17ef0dcd896
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 12:51:20 +0100

    Add concurrent test file: tests/unit_test_007.py

---
+++ tests/unit_test_007.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestUnit7(unittest.TestCase):
+    def test_addition_7(self):
+        self.assertEqual(7 + 7, 14)
+
+    def test_multiplication_7(self):
+        self.assertEqual(7 * 7, 49)
+
+    def test_division_7(self):
+        self.assertEqual(14 / 2, 7)
