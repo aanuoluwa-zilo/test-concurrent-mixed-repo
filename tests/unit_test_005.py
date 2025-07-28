50e2c5a92ad4b89e87350af625d1620a1af24362
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 12:51:17 +0100

    Add concurrent test file: tests/unit_test_005.py

---
+++ tests/unit_test_005.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestUnit5(unittest.TestCase):
+    def test_addition_5(self):
+        self.assertEqual(5 + 5, 10)
+
+    def test_multiplication_5(self):
+        self.assertEqual(5 * 5, 25)
+
+    def test_division_5(self):
+        self.assertEqual(10 / 2, 5)
