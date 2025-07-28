dfc1f7a8a5355b9261d7827cf166365785aac4c3
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 12:51:18 +0100

    Add concurrent test file: tests/unit_test_006.py

---
+++ tests/unit_test_006.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestUnit6(unittest.TestCase):
+    def test_addition_6(self):
+        self.assertEqual(6 + 6, 12)
+
+    def test_multiplication_6(self):
+        self.assertEqual(6 * 6, 36)
+
+    def test_division_6(self):
+        self.assertEqual(12 / 2, 6)
