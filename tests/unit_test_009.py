e863ac939dcd31855e961e2a2afe31977e5e5757
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 12:51:23 +0100

    Add concurrent test file: tests/unit_test_009.py

---
+++ tests/unit_test_009.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestUnit9(unittest.TestCase):
+    def test_addition_9(self):
+        self.assertEqual(9 + 9, 18)
+
+    def test_multiplication_9(self):
+        self.assertEqual(9 * 9, 81)
+
+    def test_division_9(self):
+        self.assertEqual(18 / 2, 9)
